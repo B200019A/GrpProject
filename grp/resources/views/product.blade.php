783f066a567bc52e785069ab787ccaebf28b1a6f
yueyunlow <68683756+yueyunlow@users.noreply.github.com>
2022-01-24 22:03:18 +0800

    Update product.blade.php

--- grp/resources/views/product.blade.php
+++ grp/resources/views/product.blade.php
@@ -1,43 +1,35 @@
 @extends('layouts.app')
-
-@section('content')
-    <div style="margin-top:10px; margin-bottom:10px">
-    <div class="row">
-        <div class="col-sm-2"></div>
-            <div class="col-md-8">
-            <div class="card">
-                <div class="card-title"style="margin-left:20px;"><h4>Club&Society</h4></div> 
-
-                <div style="margin-left:20px; margin-right:20px;  "><!--adjust margin strat-->
-                <div class="row">
-                
-                    @foreach($products as $product)
-    
-                    <div class="col-sm-4" style="margin-top:10px">
-                        <div class="card h-100">
-                            <div class="card-body">
-                            <h5 class="card-title">{{  $product->name }}</h5>
-                            <a href="{{route('clubProduct.detail',['id'=>$product->id])}}"><img src="{{asset('images/product/')}}/{{  $product->image }}" alt="" width="100" class="img-fluid"><a>
-                       
-                            <a>Club: {{$product->clubName}}</a>
-                            
-                            <div class="card-heading">RM {{  $product->price }}<a href="{{route('clubProduct.detail',['id'=>$product->id])}}"><button type="submit" style="float:right;" class="btn btn-danger btn-xs">View Detail</button><a></div>
+@section('content')   
+<div class="row" style="margin-top:10px; margin-bottom:10px; background: #f8f8f8;">
+    <div class="row" style="margin-top:10px; margin-bottom:10px; text-align:center;">
+        <br><br>
+        <h4>Products</h4>
+        <br><br>
+    </div>
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            @foreach($products as $product)
+            <div class="col">
+                <div class="card" style="width: 18rem;height: 25rem;">
+                    <div class="card-body d-flex flex-column mt-auto">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <input type="hidden" name="id" value="{{ $product->id }}">
+                        <a href="{{route('clubProduct.Detail',['id'=>$product->id])}}"><img src="{{asset('images/product/')}}/{{  $product->image }}" alt="" width="150" height="150" class="card-img-top"></a>
+                        <div class="card-body">
+                            <div class="card-heading">{{ $product->clubName}}</div>
+                            <br>
+                            <div class="card-heading">RM{{ $product->price }}</div>
                         </div>
-                        </div>
+                        <a href="{{route('clubProduct.Detail',['id'=>$product->id])}}"><button type="submit" style="float:right;" class="btn btn-danger btn-xs">View Detail</button><a>
                     </div>
-        
-                    @endforeach
-                    
-                    &nbsp;
                 </div>
-                </div><!--adjust margin end-->
-                </div>
+                <br>    
             </div>
-
-    
-        
-        <div class="col-sm-2"></div>
-    </div>
-    </div>
-
+            @endforeach  
+        </div>
+        <br><br>
+    </div>          
+    <div class="col-sm-2"></div>
+</div>
 @endsection
