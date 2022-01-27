a3dda0984dbd3744e1dcfe5f86eb2c86ce2f66d5
B200019A <b200019a@sc.edu.my>
2022-01-28 07:15:37 +0800

    payment background

--- grp/resources/views/payment.blade.php
+++ grp/resources/views/payment.blade.php
@@ -3,112 +3,114 @@
 
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-	<form action="{{ route('payment.post') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">      @CSRF
+	
+   <form action="{{ route('payment.post') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">      @CSRF
+   <div class="row admin-background">
     <div class="row">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <br><br>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <td>Image</td>
-                        <td>clubId</td>
-                        <td>Name</td>
-                        <td>Price</td>
-                        <td>Quantity</td>
-                        <td>Subtotal</td>
-                    </tr>
-                </thead>
-                <tbody>
-                     @foreach($orderItems as $orderItem)
-                    <tr>
-                        <input type="hidden" name="cid" id="cid" value="{{ $orderItem->cid}}">
-                        <td>
-                        <input type="hidden" name="subtotal[]" id="subtotal[]" value="{{ $orderItem->price*$orderItem->cartQty}}"> 
-                            <img src="{{ asset('images/product/')}}/{{ $orderItem->image }}" alt="" width="100" class="img-fluid"></td>
-                            <td>{{  $orderItem->clubName }}</td>
-                            <td>{{  $orderItem->name }}</td>    
-                            <td>RM{{  $orderItem->price }}</td>
-                            <td>{{  $orderItem->cartQty }}</td>
-                            <td  onclick="cal()">RM{{  $orderItem->price*$orderItem->cartQty}}</td>
-                    </tr> 
-                    @endforeach
-                    @foreach($orderAmounts as $orderAmount)
-                    <tr align="right">
-                        <td colspan="5">&nbsp;</td>
-                        <td>Total: RM<i> </i> <input type="text" value="{{$orderAmount->amount}}" name="amount" id="amount" size="7" readonly />
-                        <input type="hidden" value="{{$orderAmount->id}}" name="orderID" id="orderID" size="7" readonly />
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="col-sm-3">
-            
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-10"></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-3"></div>
-        <br>
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default credit-card-box">
-                <div class="panel-heading" >
-                    <div class="row">
-                      <h3>Card Payment</h3>
-                    
-                    </div>
-                </div>
-                <div class="panel-body">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <br><br>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <td>Image</td>
+                            <td>clubId</td>
+                            <td>Name</td>
+                            <td>Price</td>
+                            <td>Quantity</td>
+                            <td>Subtotal</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($orderItems as $orderItem)
+                        <tr>
+                            <input type="hidden" name="cid" id="cid" value="{{ $orderItem->cid}}">
+                            <td>
+                            <input type="hidden" name="subtotal[]" id="subtotal[]" value="{{ $orderItem->price*$orderItem->cartQty}}"> 
+                                <img src="{{ asset('images/product/')}}/{{ $orderItem->image }}" alt="" width="100" class="img-fluid"></td>
+                                <td>{{  $orderItem->clubName }}</td>
+                                <td>{{  $orderItem->name }}</td>    
+                                <td>RM{{  $orderItem->price }}</td>
+                                <td>{{  $orderItem->cartQty }}</td>
+                                <td  onclick="cal()">RM{{  $orderItem->price*$orderItem->cartQty}}</td>
+                        </tr> 
+                        @endforeach
+                        @foreach($orderAmounts as $orderAmount)
+                        <tr align="right">
+                            <td colspan="5">&nbsp;</td>
+                            <td>Total: RM<i> </i> <input type="text" value="{{$orderAmount->amount}}" name="amount" id="amount" size="7" readonly />
+                            <input type="hidden" value="{{$orderAmount->id}}" name="orderID" id="orderID" size="7" readonly />
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-3">
                 
-                    <br>
-                    
-                    <div class='form-row row'>
-                        <div class='col-xs-12 col-md-6 form-group required'>
-                            <label class='control-label'>Name on Card</label> 
-                            <input class='form-control' size='4' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-6 form-group required'>
-                            <label class='control-label'>Card Number</label> 
-                            <input autocomplete='off' class='form-control card-number' size='20' type='text'>
-                        </div>                           
-                    </div>                        
-                    <div class='form-row row'>
-                        <div class='col-xs-12 col-md-4 form-group cvc required'>
-                            <label class='control-label'>CVC</label> 
-                            <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-4 form-group expiration required'>
-                            <label class='control-label'>Expiration Month</label> 
-                            <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-4 form-group expiration required'>
-                            <label class='control-label'>Expiration Year</label> 
-                            <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
-                        </div>
-                    </div>
-                    {{-- <div class='form-row row'>
-                        <div class='col-md-12 error form-group hide'>
-                        <div class='alert-danger alert'>Please correct the errors and try
-                            again.
-                        </div>
-                        </div>
-                    </div> --}}
-                    <div class="form-row row">
-                        <div class="col-xs-12">
-                            <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
-                        </div>
-                    </div>
-                    
-                </div>
             </div>
         </div>
-        </form>
+        <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <br>
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default credit-card-box">
+                    <div class="panel-heading" >
+                        <div class="row">
+                        <h3>Card Payment</h3>
+                        
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                    
+                        <br>
+                        
+                        <div class='form-row row'>
+                            <div class='col-xs-12 col-md-6 form-group required'>
+                                <label class='control-label'>Name on Card</label> 
+                                <input class='form-control' size='4' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-6 form-group required'>
+                                <label class='control-label'>Card Number</label> 
+                                <input autocomplete='off' class='form-control card-number' size='20' type='text'>
+                            </div>                           
+                        </div>                        
+                        <div class='form-row row'>
+                            <div class='col-xs-12 col-md-4 form-group cvc required'>
+                                <label class='control-label'>CVC</label> 
+                                <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                                <label class='control-label'>Expiration Month</label> 
+                                <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                                <label class='control-label'>Expiration Year</label> 
+                                <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
+                            </div>
+                        </div>
+                        {{-- <div class='form-row row'>
+                            <div class='col-md-12 error form-group hide'>
+                            <div class='alert-danger alert'>Please correct the errors and try
+                                again.
+                            </div>
+                            </div>
+                        </div> --}}
+                        <div class="form-row row">
+                            <div class="col-xs-12">
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            </form>
+        </div>
     </div>
-
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 <script type="text/javascript">
 $(function() {
